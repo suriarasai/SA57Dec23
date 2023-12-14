2d872edff4c66c438dede93925d2403f45129442
suriarasai <suria.r.asai@gmail.com>
2023-12-14 13:54:47 +0800

    First Commit

---
+++ springwebfluxdemo/src/main/java/sg/edu/iss/reactordemo/SpringwebfluxdemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.reactordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxdemoApplication.class, args);
+	}
+
+}
